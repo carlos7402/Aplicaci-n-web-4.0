55c2436814c217710e5940109c3d63ceb4a54766
Juan Carlos <carloseh.ti18@utsjr.edu.mx>
2021-01-15 11:55:19 -0600

    actualizado

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express=require('express');
-const path=require('path');
-const app=express();
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use('/public',express.static(path.join(__dirname,'/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.listen(3000,()=>{
-    console.log("Servidor en el puerto 3000");
+app.listen(port, () => {
+    console.log("Servidor en el puerto " + port);
 })
